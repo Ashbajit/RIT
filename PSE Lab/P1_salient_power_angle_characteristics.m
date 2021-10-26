51ba1518d001388e4fe3ed90f870244a95a9823a
Ash_Bajit13 <48647015+Ashbajit@users.noreply.github.com>
2021-10-26 21:49:21 +0530

    Formatting

--- PSE Lab/P1_salient_power_angle_characteristics.m
+++ PSE Lab/P1_salient_power_angle_characteristics.m
@@ -1,5 +1,5 @@
 % Plot power angle characteristics of salient pole machine
-% A salient pole synchronous machine has following parameters. Zero
+% Q. A salient pole synchronous machine has following parameters. Zero
 % armature resistance, direct axis reactance of 1 p.u., quadrature ais
 % reactance of 0.6 p.u. , terminal voltage of 1 p.u., unity power factor
 % and induced emf of 1.1 p.u. Write a program to obtain power angle
@@ -7,30 +7,30 @@
 clc;
 clear all;
 %Initial parameter values
-Ra = 0;                     %Armature resistance
-Xd = 1;                     %Direct axis reactance
-Xq = 0.6;                   %Quadrature axis reactance
-Vt = 1;                      %Terminal Voltage
-Pf = 1;                     %Power factor
-E = 1.1;                    %Induced EMF
-delta = 0:1:180;            %delta varying from 0 to 180 degrees
-del_rad = delta .* pi/180;  %converting to degrees
+Ra = 0; %Armature resistance
+Xd = 1; %Direct axis reactance
+Xq = 0.6; %Quadrature axis reactance
+Vt = 1; %Terminal Voltage
+Pf = 1; %Power factor
+E = 1.1; %Induced EMF
+delta = 0:1:180; %delta varying from 0 to 180 degrees
+del_rad = delta .* pi / 180; %converting to degrees
 
 %Power calculations
-Pm1 = (abs(E).*abs(Vt))/Xd;
-y1 = Pm1.*sin(del_rad);     % power
-Pm2 = ((Vt.^2).*(Xd-Xq))/(2.*Xd.* Xq);
-y2 = Pm2.*sin(2.*del_rad);  % power
-P = y1 + y2;                %Total power
+Pm1 = (abs(E) .* abs(Vt)) / Xd;
+y1 = Pm1 .* sin(del_rad); % power
+Pm2 = ((Vt.^2) .* (Xd - Xq)) / (2 .* Xd .* Xq);
+y2 = Pm2 .* sin(2 .* del_rad); % power
+P = y1 + y2; %Total power
 
-%Plot 
+%Plot
 
-plot(delta,P,'r')
+plot(delta, P, 'r')
 hold on
-plot(delta,y1,'b')
+plot(delta, y1, 'b')
 hold on
-plot(delta,y2,'m')
+plot(delta, y2, 'm')
 xlabel('Delta(degrees)-->');
 ylabel('Power(pu)-->');
 title('Power-angle characteristics of salient pole machine');
-legend('P','Pm1sin(delta)','Pm2sin(2delta)');
+legend('P', 'Pm1sin(delta)', 'Pm2sin(2delta)');
--- PSE Lab/P2_non_salient_power_angle_characteristics.m
+++ PSE Lab/P2_non_salient_power_angle_characteristics.m
@@ -1,5 +1,5 @@
-% Power - angle characteristics of salient pole machine 
-% A non- salient pole synchronous machine has the following parameters,
+% Power - angle characteristics of salient pole machine
+% Q. A non- salient pole synchronous machine has the following parameters,
 % zero armature resistance, synchronous reactance, of 0.e p.u. , terminal
 % voltage of 1 p.u. , power factor of 0.8 and power of 0.5 p.u.. Write a
 % program to obtain the power angle characteristics of machine.
@@ -8,27 +8,27 @@ clc;
 close all;
 
 %Initial Parameters
-Ra = 0;        %Terminal Resistance
-Xs = 0.3;      %Synchronous Reactance
-Vt = 1;        %Terminal Voltage
-Pf = 0.8;      %Power Factor
-P = 0.5;       %Power
+Ra = 0; %Terminal Resistance
+Xs = 0.3; %Synchronous Reactance
+Vt = 1; %Terminal Voltage
+Pf = 0.8; %Power Factor
+P = 0.5; %Power
 
 % Calculations
 delta = 0:1:180;
-del_rad = delta.* pi/180; %conversion to radians
+del_rad = delta .* pi / 180; %conversion to radians
 
 phi = acos(Pf); %From power factor
 
-I = P/(Vt.*Pf);
-Ia = (I.*Pf)-(I.*sin(phi));
-Z = complex(Ra,Xs);
-E = V + (Ia.*Z);
+I = P / (Vt .* Pf);
+Ia = (I .* Pf) - (I .* sin(phi));
+Z = complex(Ra, Xs);
+E = V + (Ia .* Z);
 
 %Plot
-Pm = (abs(E).*abs(V))/(Xs);
-P_t = Pm.*sin(del_rad);
-plot(delta,P_t);
+Pm = (abs(E) .* abs(V)) / (Xs);
+P_t = Pm .* sin(del_rad);
+plot(delta, P_t);
 xlabel('Delta(degrees)');
 ylabel('Power(pu)');
 title('Power angle characteristics of salient pole machine');
