d563b5a27ec7d2cf70c7a0d524ac7a82f7d6611f
Ash_Bajit13 <48647015+Ashbajit@users.noreply.github.com>
2021-10-26 21:47:24 +0530

    PSE_Initialization
    First set of programs in PSE folder

---
+++ PSE Lab/P2_non_salient_power_angle_characteristics.m
@@ -0,0 +1,34 @@
+% Power - angle characteristics of salient pole machine 
+% A non- salient pole synchronous machine has the following parameters,
+% zero armature resistance, synchronous reactance, of 0.e p.u. , terminal
+% voltage of 1 p.u. , power factor of 0.8 and power of 0.5 p.u.. Write a
+% program to obtain the power angle characteristics of machine.
+
+clc;
+close all;
+
+%Initial Parameters
+Ra = 0;        %Terminal Resistance
+Xs = 0.3;      %Synchronous Reactance
+Vt = 1;        %Terminal Voltage
+Pf = 0.8;      %Power Factor
+P = 0.5;       %Power
+
+% Calculations
+delta = 0:1:180;
+del_rad = delta.* pi/180; %conversion to radians
+
+phi = acos(Pf); %From power factor
+
+I = P/(Vt.*Pf);
+Ia = (I.*Pf)-(I.*sin(phi));
+Z = complex(Ra,Xs);
+E = V + (Ia.*Z);
+
+%Plot
+Pm = (abs(E).*abs(V))/(Xs);
+P_t = Pm.*sin(del_rad);
+plot(delta,P_t);
+xlabel('Delta(degrees)');
+ylabel('Power(pu)');
+title('Power angle characteristics of salient pole machine');
